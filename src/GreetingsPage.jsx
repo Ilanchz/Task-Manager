22ba3be014c74ee60851a2344d89865159a36c5a
Ilan <vilanchezhiyan@gmail.com>
2024-03-07 18:10:36 +0530

    update greetings message

--- src/GreetingsPage.jsx
+++ src/GreetingsPage.jsx
@@ -1,6 +1,6 @@
 function GreetingsPage(props){
     return <div>
-        <h1 className="font-serif font-bold">Greetings, {" "+props.name}</h1>
+        <h1 className="font-serif font-bold">Welcome , {" "+props.name}</h1>
         <hr/>
     </div>
 }
